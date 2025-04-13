a83b2239b5b1e4cb29a932ed19afbfe3a0dd5590
YotaOkuda <yotaai200@gmail.com>
2025-04-13 23:15:06 +0900

    adjust UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { auth } from "../../firebase";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = ({ isAuth }) => {
+  const location = useLocation();
+
+  const isActive = (path) => {
+    return location.pathname === path;
+  };
 
   return (
     <nav className="bg-white shadow-lg">
@@ -18,7 +22,11 @@ const Navbar = ({ isAuth }) => {
           </div>
           <div className="flex items-center space-x-4">
             <Link
-              className="text-gray-700 hover:text-blue-600 px-3 py-2 rounded-md text-sm font-medium"
+              className={`text-gray-700 hover:text-blue-600 px-3 py-2 rounded-md text-lg font-medium  ${
+                isActive('/')
+                  ? 'text-blue-600 bg-blue-100'
+                  : 'text-gray-700 hover:text-blue-600 hover:bg-gray-50'
+              }`}
               to="/"
             >
               Home
@@ -26,13 +34,21 @@ const Navbar = ({ isAuth }) => {
             {isAuth ? (
               <>
                 <Link
-                  className="text-gray-700 hover:text-blue-600 px-3 py-2 rounded-md text-sm font-medium"
+                  className={`text-gray-700 hover:text-blue-600 px-3 py-2 rounded-md text-lg font-medium  ${
+                    isActive('/summary')
+                      ? 'text-blue-600 bg-blue-100'
+                      : 'text-gray-700 hover:text-blue-600 hover:bg-gray-50'
+                  }`}
                   to="/summary"
                 >
                   Monthly Summary
                 </Link>
                 <Link
-                  className="text-gray-700 hover:text-blue-600 px-3 py-2 rounded-md text-sm font-medium"
+                  className={`text-gray-700 hover:text-blue-600 px-3 py-2 rounded-md text-lg font-medium  ${
+                    isActive('/logout')
+                      ? 'text-blue-600 bg-blue-100'
+                      : 'text-gray-700 hover:text-blue-600 hover:bg-gray-50'
+                  }`}
                   to="/logout"
                 >
                   Logout
@@ -40,10 +56,14 @@ const Navbar = ({ isAuth }) => {
               </>
             ) : (
               <Link
-                className="text-gray-700 hover:text-blue-600 px-3 py-2 rounded-md text-sm font-medium"
+              className={`text-gray-700 hover:text-blue-600 px-3 py-2 rounded-md text-lg font-medium  ${
+                isActive('/login')
+                  ? 'text-blue-600 bg-blue-100'
+                  : 'text-gray-700 hover:text-blue-600 hover:bg-gray-50'
+              }`}
                 to="/login"
               >
-                ログイン
+                Login
               </Link>
             )}
           </div>
