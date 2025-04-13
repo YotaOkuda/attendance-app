7146fd9b72d6ba0f4d0e98cb51cea81562010e1d
YotaOkuda <yotaai200@gmail.com>
2025-04-13 23:10:21 +0900

    adjust appearance

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,25 +1,41 @@
-import { signInWithPopup } from 'firebase/auth';
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import { auth, provider } from '../../firebase';
+import { signInWithPopup } from "firebase/auth";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { auth, provider } from "../../firebase";
+import { LogIn } from "lucide-react";
 
-
-const Login = ({setIsAuth}) => {
+const Login = ({ setIsAuth }) => {
   const navigate = useNavigate();
   const loginWithGoogle = () => {
     signInWithPopup(auth, provider).then((result) => {
       localStorage.setItem("isAuth", true);
       setIsAuth(true);
-      navigate("/")
+      navigate("/");
     });
   };
 
   return (
-    <div>
-      <p>ログインして始める</p>
-      <button onClick={loginWithGoogle}>Googleでログイン</button>
+    <div className="min-h-[calc(100vh-4rem)] flex items-center justify-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+      <div>
+        <div>
+          <div className="mx-auto h-12 w-12 text-blue-600">
+            <LogIn className="h-full w-full" />
+          </div>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+        <div className="flex justify-center items-center mt-14">
+          <button
+            className="py-4 px-6 border border-transparent text-lg font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+            onClick={loginWithGoogle}
+          >
+            Sign in with Google
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/pages/Logout.jsx
+++ src/pages/Logout.jsx
@@ -2,6 +2,7 @@ import { signOut } from "firebase/auth";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../../firebase";
+import { LogOut } from "lucide-react";
 
 const Logout = ({ setIsAuth }) => {
   const navigate = useNavigate();
@@ -14,9 +15,25 @@ const Logout = ({ setIsAuth }) => {
   };
 
   return (
-    <div>
-      <p>ログアウトする</p>
-      <button onClick={logout}>ログアウト</button>
+    <div className="min-h-[calc(100vh-4rem)] flex items-center justify-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+      <div>
+        <div>
+          <div className="mx-auto h-12 w-12 text-blue-600">
+            <LogOut className="h-full w-full" />
+          </div>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Sign out of your account
+          </h2>
+        </div>
+        <div className="flex justify-center items-center mt-14">
+          <button
+            className="py-4 px-6 border border-transparent text-lg font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+            onClick={logout}
+          >
+            Sign out
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
